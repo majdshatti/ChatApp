209ec70fc37a253aa1e6fb1dac294c1245f4b3c6
majdshatti <majdshatti8@gmail.com>
2022-10-04 22:06:06 +0300

    feat: add migration table

--- laravel/database/migrations/2022_10_04_153852_create_contacts_table.php
+++ laravel/database/migrations/2022_10_04_153852_create_contacts_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,13 +13,11 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create("users", function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->id();
             $table->uuid("_id");
-            $table->string("username");
-            $table->string("email")->unique();
-            $table->string("password");
-            $table->rememberToken();
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->foreignId("owner_id")->constrained("users")->onDelete("cascade");
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists("users");
+        Schema::dropIfExists('contacts');
     }
 };
