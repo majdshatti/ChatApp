30a06ea775cb8f8b64f1fdaf67de76cc8aac966d
majdshatti <majdshatti8@gmail.com>
2022-10-04 22:04:26 +0300

    fix: changed name to username and added id to hidden fields

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -12,19 +12,20 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public $table = "users";
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = ["name", "email", "password"];
+    protected $fillable = ["_id", "username", "email", "password"];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = ["password", "remember_token"];
+    protected $hidden = ["password", "remember_token", "id"];
 
     /**
      * The attributes that should be cast.
