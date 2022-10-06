5e80848998c60e3808eb08d9397f0fc4f8e544b5
majdshatti <majdshatti8@gmail.com>
2022-10-07 00:07:53 +0300

    refactor: changed route to have a query param

--- laravel/routes/auth.php
+++ laravel/routes/auth.php
@@ -4,7 +4,13 @@ use Illuminate\Support\Facades\Route;
 /** Controllers */
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\VerifySocketController;
 
 /** Public routes */
 Route::post('/register', [RegisterController::class, 'index']);
 Route::post('/login', [LoginController::class, 'index']);
+
+/** Private routes */
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::get('/verify/{userSentToId}', [VerifySocketController::class, 'index']);
+});
