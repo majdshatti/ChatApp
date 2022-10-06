461e85ebe91c27e74e98b5df92cd355f3f43e671
majdshatti <majdshatti8@gmail.com>
2022-10-07 00:06:28 +0300

    feat: add phone_number column

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->id();
             $table->uuid("_id");
             $table->string("username");
+            $table->string("phone_number");
             $table->string("email")->unique();
             $table->string("password");
             $table->rememberToken();
