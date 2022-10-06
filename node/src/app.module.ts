c23986cf616a0c4cefb534375483388bcf3217cd
majdshatti <majdshatti8@gmail.com>
2022-10-07 00:10:26 +0300

    removed: due to framework changing

--- node/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ChatGeteway } from './chat/chat.geteway';
-
-@Module({
-  imports: [],
-  controllers: [],
-  providers: [ChatGeteway],
-})
-export class AppModule {}
