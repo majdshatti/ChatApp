a272dfd4f0a166c6f40345888043dac23cbbd9d4
majdshatti <majdshatti8@gmail.com>
2022-10-07 00:07:12 +0300

    feat: added three users for quick testing

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +17,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::insert([
+            [
+                "_id" => "976ffc0fe0794c8eb48cf447fa4f9558",
+                "username" => "majdsh",
+                "password" => bcrypt("vip12345"),
+                "phone_number" => "0937423525",
+                "email" => "majdsha@gmail.com"
+            ],[
+                "_id" => "976ffc0ff84d4896ad9a9a4c103bdd13",
+                "username" => "mhd",
+                "password" => bcrypt("vip12345"),
+                "phone_number" => "0937423526",
+                "email" => "mhda@gmail.com"
+            ],[
+                "_id" => "976ffc100df74d9085379dd29a949929",
+                "username" => "abd",
+                "password" => bcrypt("vip12345"),
+                "phone_number" => "0937423527",
+                "email" => "abda@gmail.com"
+            ]
+        ]);
     }
 }
