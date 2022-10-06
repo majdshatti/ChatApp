57e114d468a8ca8315f4dec663c6ff8cd44a4258
majdshatti <majdshatti8@gmail.com>
2022-10-07 00:02:50 +0300

    feat: added get a list of user

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Display a listing of users.
+     *
+     * @return Response
+     */
+    public function index() {
+        return okResponse([
+            "data" => User::all()
+        ]);
+    }
 }
